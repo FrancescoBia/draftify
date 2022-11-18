0538549a5571266d3abb8fb1a8a1877b654d5295
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-18 16:59:10 +0100

    add reduct toolkit and configure store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 import '../styles/editorTheme.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { store } from '../src/redux/store'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+	)
 }
