3beda24281c27ef49fa2b1aa0d43e696777943c2
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-21 13:24:17 +0100

    fix bug: todays note doesn't load when empty

--- src/redux/editableNote-slice.ts
+++ src/redux/editableNote-slice.ts
@@ -1,14 +1,31 @@
 import { createReducer, createAsyncThunk, createAction } from '@reduxjs/toolkit'
 import { _fetchNote, _saveNote } from '../controllers/firebase'
 import { fetchNote } from './notes-slice'
+import { getNoteIdFromDate } from '../utils/dateFormatter'
+import { RootState } from './store'
 
 type NoteState = Note | null
 
 export const makeNoteEditable = createAsyncThunk(
 	'editableNote/load',
-	async (noteId: Note['id'], { dispatch }) => {
-		// make sure to fetch the latest state of the note before making it editable
-		const note = dispatch(fetchNote({ noteId })).unwrap()
+	async (noteId: Note['id'], { dispatch, getState }) => {
+		// fetch the latest state of the note before making it editable
+		const note = await dispatch(fetchNote({ noteId }))
+			.unwrap()
+			// if no note is found (e.g. today's note)
+			.catch((err) => {
+				const today = getNoteIdFromDate()
+				// check if user is trying to load today's note
+				if (noteId === today) {
+					// check if it has already been loaded up (it should be created by the notes-slice reducer)
+					const todaysNote = (getState() as RootState).allNotes[today]
+					// if found, return it
+					if (todaysNote) return todaysNote
+				}
+				// otherwise, if note is not today's and/or is not found in local state, reject
+				return Promise.reject(err)
+			})
+
 		return note
 	}
 )
