4e76d757a425a203946d4f5cd5148938430c303e
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-21 21:11:04 +0100

    update Navigator selected state

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -7,15 +7,10 @@ type Props = {
 
 const Navigator = (props: Props) => {
 	const today = getNoteIdFromDate()
-	// useEffect(() => {
-	// 	console.log('navigator render')
-
-	// 	return () => {}
-	// }, [])
 
 	return (
 		<div className='h-full w-12 bg-gray-100 dark:bg-gray-900 group'>
-			<div className='absolute z-10 top-0 left-0 w-56 bg-white dark:bg-gray-900 h-screen shadow-lg hidden group-hover:block opacity-0 hover:opacity-100 transition-opacity duration-500'>
+			<div className='absolute z-10 top-0 left-0 w-56 bg-white dark:bg-gray-900 h-screen shadow-lg hidden group-hover:block opacity-0 hover:opacity-100 transition-opacity duration-500 pt-4'>
 				<div className='flex flex-col'>
 					{props.notesIdList.map((noteId) => (
 						<NoteItem
@@ -35,14 +30,14 @@ type NoteItemProps = {
 
 const NoteItem = (props: NoteItemProps) => {
 	const router = useRouter()
+	const isSelected = location.pathname.split('/')[1] === props.noteId
 
 	return (
 		<button
-			className='px-4 py-2 text-left'
-			onClick={() => {
-				// dispatch(fetchNote({ noteId: props.noteId }))
-				router.push(`/${props.noteId}`)
-			}}
+			className={`px-6 py-2 text-left ${
+				isSelected ? 'bg-gray-100 font-bold' : ''
+			}`}
+			onClick={() => router.push(`/${props.noteId}`)}
 		>
 			{props.noteId === 'today' ? 'today' : prettyFormatDate(props.noteId)}
 		</button>
