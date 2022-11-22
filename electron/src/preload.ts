07a6f5ad4e701e0ca17b8e9e3db8184cf0034946
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-22 10:36:30 +0100

    remove electron-forge and add electron-typescript template

--- electron/src/preload.ts
+++ electron/src/preload.ts
@@ -1,2 +1,14 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector: string, text: string) => {
+    const element = document.getElementById(selector);
+    if (element) {
+      element.innerText = text;
+    }
+  };
+
+  for (const type of ["chrome", "node", "electron"]) {
+    replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
+  }
+});
