68e58f33862df39eeb74e106e440a6bce5311480
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-23 21:19:35 +0100

    add 404 on web

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -3,21 +3,33 @@ import '../styles/editorTheme.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Provider } from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { store } from '../src/redux/store'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+	const router = useRouter()
+	const [isElectron, setIsElectron] = useState(false)
+
+	useEffect(() => {
+		if (!window?.electronAPI) router.push('/404', undefined, { shallow: true })
+		setIsElectron(!!window?.electronAPI)
+	}, [router])
+
 	return (
 		<Provider store={store}>
-			<WorkspaceLayout>
-				<Component {...pageProps}>
-					<Head>
-						<title>Journal</title>
-						<meta name='description' content='My journal app' />
-						<link rel='icon' href='/favicon.ico' />
-					</Head>
-				</Component>
-			</WorkspaceLayout>
+			{isElectron ? (
+				<WorkspaceLayout>
+					<Component {...pageProps} />
+				</WorkspaceLayout>
+			) : (
+				<Component {...pageProps} />
+			)}
+			<Head>
+				<title>Journal</title>
+				<meta name='description' content='My journal app' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 		</Provider>
 	)
 }
@@ -26,9 +38,11 @@ import { useAppDispatch, useAppSelector } from '../src/redux/hooks'
 import { fetchAllNotes } from '../src/redux/notes-slice'
 import { Navigator } from '../src/components/Navigator'
 import { Spinner } from '../src/components/Spinner'
+
 type WorkspaceLayoutProps = {
 	children: React.ReactNode
 }
+
 function WorkspaceLayout(props: WorkspaceLayoutProps) {
 	const dispatch = useAppDispatch()
 	const { allNotes } = useAppSelector((s) => s)
@@ -39,6 +53,8 @@ function WorkspaceLayout(props: WorkspaceLayoutProps) {
 
 	return (
 		<div className='h-screen flex'>
+			<div className='webkit-app-drag fixed h-7 w-full' />
+
 			{/* check that initial data has been fetched */}
 			{Object.keys(allNotes) && Object.keys(allNotes).length > 0 ? (
 				<>
--- app/pages/today.tsx
+++ app/pages/today.tsx
@@ -3,19 +3,8 @@ import { _saveNote } from '../src/controllers/firebase'
 import { getNoteIdFromDate } from '../src/utils/dateFormatter'
 import EditableNote from '../src/components/EditableNote'
 
-type Props = {}
-
-export default function Today(props: Props) {
+export default function Today() {
 	const [today] = useState(getNoteIdFromDate())
 
-	return (
-		<>
-			{window?.electronAPI ? (
-				<div className='webkit-app-drag fixed h-7 w-full' />
-			) : (
-				<></>
-			)}
-			<EditableNote noteId={today} />
-		</>
-	)
+	return <EditableNote noteId={today} />
 }
