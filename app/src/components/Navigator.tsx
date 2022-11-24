deeb5977671dd7ebdca06a23736f3f517fffea59
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-24 17:58:28 +0100

    add icon button

--- app/src/components/Navigator.tsx
+++ app/src/components/Navigator.tsx
@@ -1,5 +1,8 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { getNoteIdFromDate, prettyFormatDate } from '../utils/dateFormatter'
+import today_icon from '../assets/images/today_icon.png'
 
 type Props = {
 	notesIdList: Array<Note['id']>
@@ -9,7 +12,7 @@ const Navigator = (props: Props) => {
 	const today = getNoteIdFromDate()
 
 	return (
-		<div className='w-28'>
+		<div className='p-4'>
 			{/* <div className='absolute z-10 top-0 left-0 w-56 bg-white dark:bg-gray-900 h-screen shadow-lg hidden group-hover:block opacity-0 hover:opacity-100 transition-opacity duration-500 pt-4'>
 				<div className='flex flex-col'>
 					{props.notesIdList.map((noteId) => (
@@ -20,11 +23,25 @@ const Navigator = (props: Props) => {
 					))}
 				</div>
 			</div> */}
-			<div className='bg-gray-100 dark:bg-gray-900 flex flex-col gap-4'>
-				<button>Today</button>
-				<button>All notes</button>
+			<div className='bg-gray-100 dark:bg-gray-900 flex flex-col p-2 rounded-3xl '>
+				<IconButton href='/today' />
+				<IconButton href='/all' />
 			</div>
 		</div>
+	)
+}
+
+type ButtonProps = {
+	href: string
+}
+const IconButton = (props: ButtonProps) => {
+	return (
+		<Link
+			href={props.href}
+			className='w-14 p-2 rounded-2xl dark:hover:bg-gray-800'
+		>
+			<Image src={today_icon} alt='' />
+		</Link>
 	)
 }
 
