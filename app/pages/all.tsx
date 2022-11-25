d85413a4d6a7272f2cf759a9ab0fdd09d6a8ca30
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-25 10:20:21 +0100

    improve all notes page empty state

--- app/pages/all.tsx
+++ app/pages/all.tsx
@@ -7,12 +7,16 @@ export default function AllNotes(props: AllNotesProps) {
 	const allNotes = useContext(NotesContext)
 
 	return (
-		<div>
-			{!allNotes
-				? 'loading'
-				: Object.values(allNotes).length < 1
-				? 'no notes found'
-				: Object.values(allNotes)[0].id}
-		</div>
+		<>
+			{!allNotes ? (
+				'loading'
+			) : Object.values(allNotes).length < 1 ? (
+				<div className='h-full w-full flex items-center justify-center text-gray-500'>
+					You don&apos;t have any older notes... Yet!
+				</div>
+			) : (
+				Object.values(allNotes)[0].id
+			)}
+		</>
 	)
 }
