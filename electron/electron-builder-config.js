71e5beb46b6683834d79e2c15efc795f0d99d0a8
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-25 19:37:23 +0100

    create basic electron build

---
+++ electron/electron-builder-config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+
+// require('dotenv').config()
+
+module.exports = () => {
+	const isProd = process.env.NODE_ENV === 'production'
+
+	return {
+		appId: 'app.draftify.app',
+		mac: {
+			icon: './assets/app_icon.icns',
+			category: 'public.app-category.productivity',
+		},
+		files: [
+			'dist/**/*',
+			'package.json',
+			{
+				from: 'build/prod/',
+				to: '',
+				filter: ['**/*'],
+			},
+		],
+	}
+}
+// return {
+// 	appId: 'app.draftify.app',
+// 	productName: 'draftify',
+// 	artifactName: 'draftify-${version}-${os}-${arch}.${ext}',
+// 	directories: {
+// 		output: `dist/${isProd ? 'prod' : 'dev' + '/${buildVersion}'}/main.js`, // ${buildversion} is Electron-builder syntax
+// 		buildResources: 'build',
+// 	},
+// 	generateUpdatesFilesForAllChannels: true,
+// 	files: [`build/${isProd ? 'prod' : 'dev'}/**/*`],
+// 	// publish: [
+// 	// 	{
+// 	// 		provider: 'spaces',
+// 	// 		name: 'app-dist-files',
+// 	// 		region: 'fra1', // https://docs.digitalocean.com/products/platform/availability-matrix/
+// 	// 		channel: '${channel}',
+// 	// 	},
+// 	// 	{
+// 	// 		provider: 'github',
+// 	// 		private: true,
+// 	// 		owner: 'FrancescoBia',
+// 	// 		token: process.env.GITHUB_TOKEN,
+// 	// 	},
+// 	// ],
+// 	protocols: {
+// 		name: 'draftify',
+// 		schemes: ['draftify'],
+// 		// schemes: [
+// 		// 	process.env.CHANNEL === '' || process.env.CHANNEL === 'latest'
+// 		// 		? 'substratum'
+// 		// 		: `substratum-${process.env.CHANNEL}`,
+// 		// ],
+// 	},
+// 	mac: {
+// 		icon: './assets/app_icon.icns',
+// 		category: 'public.app-category.productivity',
+// 		hardenedRuntime: true,
+// 		darkModeSupport: true,
+// 		entitlements: 'entitlements.plist',
+// 		entitlementsInherit: 'entitlements.plist',
+// 		target: [
+// 			{
+// 				target: 'dmg',
+// 				arch: ['x64', 'arm64'],
+// 			},
+// 			{
+// 				target: 'zip',
+// 				arch: ['x64', 'arm64'],
+// 			},
+// 		],
+// 	},
+// }
