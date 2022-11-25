b6d063c201e65b9f71435cea2bc6865971583211
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-25 20:30:29 +0100

    add notarize script

---
+++ electron/notarize.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const { notarize } = require('electron-notarize')
+
+exports.default = async (context) => {
+	const { electronPlatformName, appOutDir } = context
+	if (electronPlatformName !== 'darwin') {
+		return
+	}
+
+	const appName = context.packager.appInfo.productFilename
+
+	return await notarize({
+		tool: 'notarytool',
+		appPath: `${appOutDir}/${appName}.app`,
+		appleId: process.env.APPLE_ID_EMAIL,
+		appleIdPassword: process.env.APPLE_ID_PASSWORD,
+		teamId: process.env.APPLE_TEAM_ID,
+	})
+}
