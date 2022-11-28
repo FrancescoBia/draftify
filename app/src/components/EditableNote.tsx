8a4760a9a65c2cda635241e644c0dcdec516c868
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-28 21:13:50 +0100

    tried a thing, didn't work (for now)

--- app/src/components/EditableNote.tsx
+++ app/src/components/EditableNote.tsx
@@ -115,22 +115,6 @@ export default function EditableNote({ noteId }: NoteProps) {
 							</div>
 						)}
 					</div>
-					{/* <div className='p-4 flex justify-between items-center'>
-						<p className='text-gray-500'>{prettyFormatDate(noteId)}</p>
-						<div className='flex items-center gap-6'>
-							<p className='text-gray-500'>
-								{progressSaved ? '✔︎ saved' : 'saving...'}
-							</p>
-							<button
-								onClick={() => router.push(`/${noteId}`)}
-								type='button'
-								className='text-gray-900 flex gap-2 items-center bg-white focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 fill-gray-900 dark:fill-white'
-							>
-								<Lock />
-								<p className=''>Lock</p>
-							</button>
-						</div>
-					</div> */}
 					<div className='grow p-4'>
 						<Editor
 							key={`${noteId}-editor`}
