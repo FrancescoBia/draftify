63db55683bc6182b92ab675ea04f2cbc5b5339eb
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-28 20:28:55 +0100

    add basic markdown guide

--- app/src/components/EditableNote.tsx
+++ app/src/components/EditableNote.tsx
@@ -80,6 +80,8 @@ export default function EditableNote({ noteId }: NoteProps) {
 		return () => debounceSaveData.flush()
 	}, [noteId, debounceSaveData])
 
+	const [showMarkdownLegend, setShowMarkdownLegend] = useState(false)
+
 	return (
 		<div className='grow flex h-full overflow-y-scroll justify-center'>
 			{isLoading ? (
@@ -88,6 +90,31 @@ export default function EditableNote({ noteId }: NoteProps) {
 				'error'
 			) : (
 				<div className='max-w-2xl grow flex flex-col'>
+					<div className='absolute right-0 m-4 flex items-end flex-col z-10'>
+						<button
+							className='w-8 h-8 rounded-xl p-0.5 text-center aspect-square dark:text-gray-500 border dark:border-gray-700'
+							onClick={() => setShowMarkdownLegend((s) => !s)}
+						>
+							Aa
+						</button>
+						{showMarkdownLegend && (
+							<div className='dark:bg-gray-900 bg-gray-100 p-4 mt-4 rounded-xl'>
+								<div className='mb-2 text-lg'>Markdown guide</div>
+								<div className='flex flex-col items-start gap-2'>
+									<LegendItem label='# Headline 1' />
+									<LegendItem label='## Headline 2' />
+									<LegendItem label='### Headline 2' />
+									<LegendItem label='**bold text**' />
+									<LegendItem label='*italic text*' />
+									<LegendItem label='> blockquote' />
+									<LegendItem label='- bullet list' />
+									<LegendItem label='1. numbered list' />
+									<LegendItem label='-[] checklist' />
+									<LegendItem label='-[x] checklist' />
+								</div>
+							</div>
+						)}
+					</div>
 					{/* <div className='p-4 flex justify-between items-center'>
 						<p className='text-gray-500'>{prettyFormatDate(noteId)}</p>
 						<div className='flex items-center gap-6'>
@@ -113,6 +140,14 @@ export default function EditableNote({ noteId }: NoteProps) {
 					</div>
 				</div>
 			)}
+		</div>
+	)
+}
+
+const LegendItem = (props: { label: string }) => {
+	return (
+		<div className='font-mono text-gray-500 dark:bg-gray-800 bg-gray-200  px-2 py-1 rounded text-sm'>
+			{props.label}
 		</div>
 	)
 }
