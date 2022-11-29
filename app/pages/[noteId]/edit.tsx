8658317496a891e81a3657353f469d64bab0d2a9
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-29 20:34:35 +0100

    fix bug: app crashes when you try to edit an older note

--- app/pages/[noteId]/edit.tsx
+++ app/pages/[noteId]/edit.tsx
@@ -27,39 +27,44 @@ export default function EditPage(props: Props) {
 			<div className='max-w-2xl mx-auto h-full'>
 				<EditableNote noteId={noteId} />
 			</div>
-			{window.electronAPI && window.electronAPI.appVersion() > '1.0.3' && (
-				<AlertDialog.Root>
-					<AlertDialog.Trigger asChild>
-						<button className='fixed bottom-0 right-0 m-4 flex justify-center items-center w-8 h-8 bg-red-100 dark:bg-red-900 text-red-500 dark:text-red-400 rounded-xl border border-red-200 dark:border-red-800 hover:bg-red-200 group dark:hover:bg-red-800'>
-							<div className='absolute hidden group-hover:block right-full top-0 whitespace-nowrap mr-2 bg-gray-100 dark:bg-gray-900 rounded-xl px-3 py-1 text-secondary text-sm h-full'>
-								Delete note
-							</div>
-							<TrashIcon />
-						</button>
-					</AlertDialog.Trigger>
-					<AlertDialog.Portal>
-						<AlertDialog.Overlay className='dialog-overlay' />
-						<AlertDialog.Content className='dialog-content'>
-							<AlertDialog.Title className='text-primary text-lg'>
-								Do you want to delete this note?
-							</AlertDialog.Title>
-							<AlertDialog.Description className='text-secondary mb-8 mt-2'>
-								This action cannot be undone.
-							</AlertDialog.Description>
-							<div className='flex gap-4 justify-end'>
-								<AlertDialog.Cancel asChild>
-									<button className='button-secondary'>Go back</button>
-								</AlertDialog.Cancel>
-								<AlertDialog.Action asChild>
-									<button className='button-destroy' onClick={handleDeleteNote}>
-										Yes, delete note
-									</button>
-								</AlertDialog.Action>
-							</div>
-						</AlertDialog.Content>
-					</AlertDialog.Portal>
-				</AlertDialog.Root>
-			)}
+			{window.electronAPI &&
+				window.electronAPI.appVersion &&
+				window.electronAPI.appVersion() > '1.0.3' && (
+					<AlertDialog.Root>
+						<AlertDialog.Trigger asChild>
+							<button className='fixed bottom-0 right-0 m-4 flex justify-center items-center w-8 h-8 bg-red-100 dark:bg-red-900 text-red-500 dark:text-red-400 rounded-xl border border-red-200 dark:border-red-800 hover:bg-red-200 group dark:hover:bg-red-800'>
+								<div className='absolute hidden group-hover:block right-full top-0 whitespace-nowrap mr-2 bg-gray-100 dark:bg-gray-900 rounded-xl px-3 py-1 text-secondary text-sm h-full'>
+									Delete note
+								</div>
+								<TrashIcon />
+							</button>
+						</AlertDialog.Trigger>
+						<AlertDialog.Portal>
+							<AlertDialog.Overlay className='dialog-overlay' />
+							<AlertDialog.Content className='dialog-content'>
+								<AlertDialog.Title className='text-primary text-lg'>
+									Do you want to delete this note?
+								</AlertDialog.Title>
+								<AlertDialog.Description className='text-secondary mb-8 mt-2'>
+									This action cannot be undone.
+								</AlertDialog.Description>
+								<div className='flex gap-4 justify-end'>
+									<AlertDialog.Cancel asChild>
+										<button className='button-secondary'>Go back</button>
+									</AlertDialog.Cancel>
+									<AlertDialog.Action asChild>
+										<button
+											className='button-destroy'
+											onClick={handleDeleteNote}
+										>
+											Yes, delete note
+										</button>
+									</AlertDialog.Action>
+								</div>
+							</AlertDialog.Content>
+						</AlertDialog.Portal>
+					</AlertDialog.Root>
+				)}
 		</div>
 	)
 }
