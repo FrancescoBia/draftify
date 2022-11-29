00d1c82d18672cb7d8820bd158492b366788e743
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-29 15:41:34 +0100

    add possibility to delete a note

--- app/pages/[noteId]/edit.tsx
+++ app/pages/[noteId]/edit.tsx
@@ -1,21 +1,33 @@
+import { useContext } from 'react'
 import { useRouter } from 'next/router'
+import { NotesContext } from '../_app'
 import EditableNote from '../../src/components/EditableNote'
 import TrashIcon from '../../src/assets/trash-icon.svg'
 
 type Props = {}
 
 export default function EditPage(props: Props) {
-	const { query } = useRouter()
+	const { setAllNotes } = useContext(NotesContext)
+	const router = useRouter()
+	const noteId = router.query.noteId as Note['id']
 
 	return (
 		<div className='grow'>
 			<div className='max-w-2xl mx-auto h-full'>
-				<EditableNote noteId={query.noteId as Note['id']} />
+				<EditableNote noteId={noteId} />
 			</div>
-			{window.electronAPI!.appVersion() > '1.0.3' && (
+			{window.electronAPI && window.electronAPI.appVersion() > '1.0.3' && (
 				<button
 					className='fixed bottom-0 right-0 m-4 flex justify-center items-center w-8 h-8 bg-red-100 text-red-500 rounded-xl border border-red-200 hover:bg-red-200 group'
-					onClick={() => {}}
+					onClick={() => {
+						window.electronAPI!.deleteNote({ noteId }).then(() => {
+							setAllNotes!((notes) => {
+								const { [noteId]: omit, ...remainingNotes } = notes
+								return remainingNotes
+							})
+							router.push('/all')
+						})
+					}}
 				>
 					<div className='absolute hidden group-hover:block right-full top-0 whitespace-nowrap mr-2 bg-gray-100 rounded-xl px-3 py-1 text-gray-600 text-sm h-full'>
 						Delete note
