991d8f256fe3c2d60f563c8e744d45c956bce26f
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2022-11-29 15:21:42 +0100

    add note delete button (hidden)

--- app/pages/[noteId]/edit.tsx
+++ app/pages/[noteId]/edit.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import EditableNote from '../../src/components/EditableNote'
+import TrashIcon from '../../src/assets/trash-icon.svg'
 
 type Props = {}
 
@@ -7,13 +8,19 @@ export default function EditPage(props: Props) {
 	const { query } = useRouter()
 
 	return (
-		<div className='max-w-2xl grow flex flex-col mx-auto'>
-			{/* <div className='p-4 flex justify-between items-center'>
-				<button onClick={() => {}} type='button' className='button-secondary'>
-					<p className=''>Re-lock</p>
-				</button>
-			</div> */}
-			<EditableNote noteId={query.noteId as Note['id']} />
+		<div className='grow'>
+			<div className='max-w-2xl mx-auto h-full'>
+				<EditableNote noteId={query.noteId as Note['id']} />
+			</div>
+			{/* <button
+				className='fixed bottom-0 right-0 m-4 flex justify-center items-center w-8 h-8 bg-red-100 text-red-500 rounded-xl border border-red-200 hover:bg-red-200 group'
+				onClick={() => {}}
+			>
+				<div className='absolute hidden group-hover:block right-full top-0 whitespace-nowrap mr-2 bg-gray-100 rounded-xl px-3 py-1 text-gray-600 text-sm h-full'>
+					Delete note
+				</div>
+				<TrashIcon />
+			</button> */}
 		</div>
 	)
 }
