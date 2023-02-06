a1b54a27009b8760da9c4bef820c6020fddb5a27
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2023-02-06 17:56:46 +0100

    cleanup SelectVault

--- client/src/views/SelectVault.tsx
+++ client/src/views/SelectVault.tsx
@@ -1,11 +1,10 @@
-// import HelpCircle from '../src/assets/help-circle.svg'
+import Logo from '../assets/images/logo.png'
 
 export default function SelectVault() {
 	return (
-		<div className='flex flex-col items-center justify-center grow bg-secondary'>
+		<div className='flex flex-col items-center justify-center grow bg-secondary text-center'>
 			<div className='w-24 my-4'>
-				Image should go here
-				{/* <Image src={Logo} alt='draftify logo' className='drop-shadow-icon' /> */}
+				<img src={Logo} alt='draftify logo' className='drop-shadow-icon' />
 			</div>
 			<div className='p-4 rounded-xl max-w-sm'>
 				<h2 className='text-secondary text-sm'>
@@ -37,19 +36,12 @@ export default function SelectVault() {
 						Choose an existing Draftify vault in a folder
 					</p>
 				</button>
-				<div className='bg-purple-100 text-purple-700 rounded-lg p-2 px-3 text-sm mt-10 flex gap-x-3 items-center'>
-					<div className='w-6'>
-						{/* <HelpCircle /> */}
-						??
-					</div>
-					<p className=''>
-						If you previously created documents without a vault, these will be
-						automatically migrated.
-					</p>
-				</div>
 			</div>
-			<a href='https://tally.so/r/wArxyk' className='text-sm text-tertiary'>
-				Contact support
+			<a
+				href='https://tally.so/r/wArxyk'
+				className='text-sm text-tertiary hover:text-gray-700 font-medium'
+			>
+				Support
 			</a>
 		</div>
 	)
