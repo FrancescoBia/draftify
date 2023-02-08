b7a3ebc1f33ede24d5b296d11a002a0480ee7c8e
Francesco Bianchi <9106253+FrancescoBia@users.noreply.github.com>
2023-02-08 21:11:20 +0100

    add basic Features and Footer blocks

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,8 +1,13 @@
 import Hero from '../src/components/Hero'
+import Features from '../src/components/Features'
+import Footer from '../src/components/Footer'
+
 export default function Home() {
 	return (
 		<>
 			<Hero />
+			<Features />
+			<Footer />
 		</>
 	)
 }
